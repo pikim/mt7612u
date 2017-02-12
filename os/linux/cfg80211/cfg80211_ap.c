c62f68cbe0e4490d5b7c705d24738b8c6bcc1a78
pikim <pikim@web.de>
2017-02-12 19:52:53 +0100

    Further reduced the number of warnings.

--- os/linux/cfg80211/cfg80211_ap.c
+++ os/linux/cfg80211/cfg80211_ap.c
@@ -102,7 +102,7 @@ static VOID CFG80211DRV_UpdateApSettingFromBeacon(struct rtmp_adapter *pAd, UINT
 	/* if it doesn't find WPA_IE in tail first 30 bytes. treat it as is not found */
 	wpa_ie = cfg80211_find_ie(CFG_WPA_EID, pBeacon->beacon_tail, pBeacon->beacon_tail_len);
 	rsn_ie = cfg80211_find_ie(WLAN_EID_RSN, pBeacon->beacon_tail, pBeacon->beacon_tail_len);
-	wmm_ie = cfg80211_find_vendor_ie(WFA_OUI, WMM_OUI_TYPE, pBeacon->beacon_tail, pBeacon->beacon_tail_len);
+	wmm_ie = (UCHAR*)cfg80211_find_vendor_ie(WFA_OUI, WMM_OUI_TYPE, pBeacon->beacon_tail, pBeacon->beacon_tail_len);
 	ht_cap = cfg80211_find_ie(WLAN_EID_HT_CAPABILITY, pBeacon->beacon_tail, pBeacon->beacon_tail_len);
 	ht_info = cfg80211_find_ie(CFG_HT_OP_EID, pBeacon->beacon_tail, pBeacon->beacon_tail_len);
 
@@ -580,7 +580,7 @@ BOOLEAN CFG80211DRV_ApKeyAdd(
 {
 #ifdef CONFIG_AP_SUPPORT
     CMD_RTPRIV_IOCTL_80211_KEY *pKeyInfo;
-	MAC_TABLE_ENTRY *pEntry;
+	MAC_TABLE_ENTRY *pEntry = NULL;
 	PMULTISSID_STRUCT pMbss = &pAd->ApCfg.MBSSID[MAIN_MBSSID];
 	struct rtmp_wifi_dev *pWdev = &pMbss->wdev;
 	UINT8 Wcid;
